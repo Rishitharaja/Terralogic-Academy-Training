0b221e4646a2414b38a377c0170cecc1eb6c84ea
pavan <pavankoramutla099@gmail.com>
2025-11-08 09:58:47 +0530

    Removed

--- routings-learn/src/Post.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-import React from "react";
-
-export const Post = () => {
-  const { id } = useParams();
-  return <div>PostID:{id}</div>;
-};
-export default Post;
